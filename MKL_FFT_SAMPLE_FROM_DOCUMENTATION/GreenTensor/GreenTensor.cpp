ed669df7b4582afb57df0dddf0f3ded5f008e255
Well 1306 <well1306@list.ru>
2023-05-01 09:17:26 +0300

    123

--- MKL_FFT_SAMPLE_FROM_DOCUMENTATION/GreenTensor/GreenTensor.cpp
+++ MKL_FFT_SAMPLE_FROM_DOCUMENTATION/GreenTensor/GreenTensor.cpp
@@ -3,69 +3,95 @@
 #include "Hankel-transform.cpp"
 #include <stdlib.h>
 #include <complex>
+#include <cmath>
 
-const std::complex<long double> mu0 = 1.25663706 * 1.0E-6;
-const std::complex<long double> eps0 = 8.8541878e-12;
-const std::complex<long double> lambda_w = 500 * 1.0E-9;
+
+const double cLight = 299792458;
+const double sqrt_eps0mu0 = (long double)1.0 / cLight;
+const double mu0 = 4 * PI * 1.0E-7;
+const double eps0 = 8.8541878e-12;
+const double lambda_w = 500 * 1.0E-9;
 //const std::complex<long double> eps_up = (long double)1.4 * eps0; //esp в верхнем полупространстве
 //const std::complex<long double> eps1 = ((long double)-0.33564 * eps0, (long double)-2.9236 * eps0);
 //const std::complex<long double> eps2 = (long double)1.2 * eps0;
 //const std::complex<long double> eps3 = (long double)3.0 * eps0;
 //const std::complex<long double> epsp1 = (long double)2.0 * eps0;
 //const std::complex<long double> epsp2 = ((long double)-0.33564 * eps0, (long double)-2.9236 * eps0);
-const std::complex<long double> eps_up = (long double)1.4; //esp в верхнем полупространстве
-const std::complex<long double> eps1 = ((long double)-0.33564, (long double)-2.9236);
-const std::complex<long double> eps2 = (long double)1.2;
-const std::complex<long double> eps3 = (long double)3.0;
-const std::complex<long double> epsp1 = (long double)2.0;
-const std::complex<long double> epsp2 = ((long double)-0.33564, (long double)-2.9236);
+const double eps_up = 1.4; //esp в верхнем полупространстве
+const std::complex<double> eps1 = (-0.33564, -2.9236);
+const double eps2 = (long double)1.2;
+const double eps3 = (long double)1.5;
+const std::complex<double> epsp1 = 2.0;
+const std::complex<double> epsp2 = (-0.33564, -2.9236);
 std::complex<long double> k0 = (long double)2 * PI / lambda_w; //k в верхнем полупространстве
-std::complex<long double> k1 = k0 * sqrt(eps1 / eps_up);
-std::complex<long double> k2 = k0 * sqrt(eps2 / eps_up);
-std::complex<long double> k3 = k0 * sqrt(eps3 / eps_up);
-std::complex<long double> kp1 = k0 * sqrt(epsp1 / eps_up);
-std::complex<long double> kp2 = k0 * sqrt(epsp2 / eps_up);
-
-std::complex<long double> z0 = (long double)2 * lambda_w;
-std::complex<long double> z1 = (long double)-3.5 * lambda_w;
-std::complex<long double> z2 = (long double)-5 * lambda_w;
 
 
+double k1;    // верхнее полупространстве подложки
+std::complex<double> k2;   // металлический слой
+std::complex<double> k3;   // второй слой
+std::complex<double> k4;   // нижнее полупространство
+std::complex<double> kp1;  // верхнее полупространство частицы 
+std::complex<double> kp2;  // нижнее  
+double k1n;    // верхнее полупространстве подложки
+std::complex<double> k2n;   // металлический слой
+std::complex<double> k3n;   // второй слой
+std::complex<double> k4n;   // нижнее полупространство
+std::complex<double> kp1n;  // верхнее полупространство частицы 
+std::complex<double> kp2n;  // нижнее полупространство частицы 
 
-std::complex<long double> eta(std::string m, long double l)
+double ro;
+double z0;
+double z1;
+const int nro = 50;
+const double roMin = 0;
+const double roMax = 10;
+
+std::complex<double> veps[6] =
+{ std::complex<double>(eps_up, 0),
+  std::complex<double>(-0.33564, 2.9236),
+  std::complex<double>(eps_up * 1.2, 0),
+  std::complex<double>(eps_up * 1.5, 0),
+  std::complex<double>(eps_up * 2.0, 0),            // epsp1
+  std::complex<double>(-0.33564, 2.9236)        // epsp2
+};
+
+void U_hs(double tau,  double& resRe, double& resIm)
 {
-	std::complex<double> r;
-	if (m == "0")
-		r = sqrt(pow(l, 2) - pow(k0, 2));
-	else if (m == "1")
-		r = sqrt(pow(l, 2) - pow(k1, 2));
-	else if(m == "2")
-		r = sqrt(pow(l, 2) - pow(k2, 2));
-	else if(m =="3")
-		r = sqrt(pow(l, 2) - pow(k3, 2));
-	else if (m == "p1")
-		r = sqrt(pow(l, 2) - pow(kp1, 2));
-	else if (m == "p2")
-		r = sqrt(pow(l, 2) - pow(kp2, 2));
-	return r;
-}
+	std::complex<double> tmp = tau * tau - kp1n * kp1n;
+	std::complex<double> eta1 = sqrt(tmp);
+
+	std::complex<double> ctmp = tau * tau - kp2n * kp2n;
+	std::complex<double> eta2 = sqrt(ctmp);
+	ctmp = 2.0 / (eta1 + eta2);
+	std::complex<double> e1 = 0;
+
+	if (z0 > 0)
+	{
+		if (z1 > 0)
+		{
+			e1 = ctmp * std::exp(-eta1 * (z0 + z1));
+		}
+		else
+		{
+			e1 = ctmp * std::exp(-eta1 * z0) * std::exp(eta2 * z1);
+		}
+
+	}
+	else
+	{
+		if (z > 0)
+		{
+			e1 = ctmp * std::exp(-eta1 * z1) * std::exp(eta2 * z0);
+		}
+		else
+		{
+			e1 = ctmp * std::exp(eta2 * z0) * std::exp(eta2 * z1);
+		}
+	}
 
 
-std::complex<long double> r2(long double l)
-{
-	return exp(eta("2", l) * z2) * (eta("2", l) - eta("3", l)) / (eta("2", l) + eta("3", l));
-}
-
-std::complex<long double> r1(long double l)
-{
-	return exp(eta("0", l) * z1) * ((eta("1", l) + eta("2", l)) * r2(l) * exp(eta("2", l) * z2)
-		+ (eta("1", l) - eta("2", l)) * exp((long double)2.0 * eta("2", l) * z1)) / ((eta("1", l) - eta("2", l)) * r2(l) * exp(eta("2", l) * z2) + (eta("1", l) + eta("2", l)) * exp((long double)2.0 * eta("2", l) * z1));
-}
-
-std::complex<long double> u(long double l, long double z)
-{
-	return exp(-eta("0", l) * (abs(z - z0))) / eta("0", l) - exp(-eta("0", l) * (z + z0)) / eta("0", l) + exp(-eta("0", l) * (z + z0)) * ((long double)2.0 * (r1(l) * exp(eta("1", l) * z1) + (long double)1.0))
-		/ (eta("0", l) * (r1(l) * exp(eta("1", l) * z1) + (long double)1.0) - eta("1", l) * (r1(l) * exp(eta("1", l) * z1) - (long double)1.0));
+	resRe = tau * real(e1);        // Re
+	resIm = tau * imag(e1);   // Im
 }
 
 struct dot2D
@@ -95,20 +121,108 @@ double GetRandomNumberFloat(long double min, long double max, int precision)
 
 int main() {
 
+	std::complex<double>* kw = new std::complex<double>[6];
+
+	double omega = 2.0 * PI * cLight / lambda_w / sqrt(eps_up);
+
+	for (int j = 0; j < 6; j++)
+		kw[j] = omega * sqrt_eps0mu0 * sqrt(veps[j]);
+
+	k1 = real(kw[0]);
+	k2 = kw[1];
+	k3 = kw[2];
+	k4 = kw[3];
+	kp1 = kw[4];
+	kp2 = kw[5];
+
+	k1n = 1;
+	k2n = k2 / k1;
+	k3n = k3 / k1;
+	k4n = k4 / k1;
+	kp1n = kp1 / k1;
+	kp2n = kp2 / k1;
+
+
+	z0 = 0.01;
+	z1 = 0.1;
+
+	double* ro = new double[nro];
+	double hro = (roMax - roMin) / nro;
+
+	for (int j = 0; j < nro; j++)
+	{
+		ro[j] = roMin + j * hro;
+	}
+
+
+	double* resRe = new double[nro];
+	double* resIm = new double[nro];
+	double* resMod = new double[nro];
+
+
+	double re = 0;
+	double im = 0;
+
+	for (int j = 0; j < nro; j++)
+	{
+		U_hs(ro[j], re, im);
+		resRe[j] = re;
+		resIm[j] = im;
+		resMod[j] = sqrt(re * re + im * im);
+	}
+
+	std::vector<double> filter = MakeFilter(hro, roMax, 0);
+
+
+	for (int j = 0; j < nro; j++)
+	{
+		std::cout << j << ": " << filter[j] << "\n";
+	}
+
+	std::vector<double> hankel = HankelTransform(filter, hro, roMax);
+
+
+
+
+
+	std::ofstream out1;          // поток для записи
+	out1.open("test.txt");
+
+	out1 << "en\n###\nЗависимость электромагнитного поля от коэффицента z\nРасстояние\nЭлектромагнитное поле\n100\n";
+	for (int i = 0; i < nro; i++)
+	{
+		out1 << ro[i] << '\n';
+	}
+	out1 << "real" << '\n';
+	for (int i = 0; i < nro; i++)
+	{
+		out1 << resRe[i] << '\n';
+	}
+	out1 << "imag" << '\n';
+	for (int i = 0; i < nro; i++)
+	{
+		out1 << resIm[i] << '\n';
+	}
+	out1 << "mod" << '\n';
+	for (int i = 0; i < nro; i++)
+	{
+		out1 << resMod[i] << '\n';
+	}
+
 	// Частица
 
-	long double PartSize = (long double)3 * real(lambda_w);
+	long double PartSize = (long double)3 * lambda_w;
 	std::vector<dot2D> RotGraph;
 	RotGraph.resize(30);
 	for (int i = 1; i < 29; i++)
 	{
 		RotGraph[i].z = PartSize / 30 * (i + 1);
-		RotGraph[i].r = GetRandomNumberFloat(1, real(lambda_w) / (long double)4, 2);
+		RotGraph[i].r = GetRandomNumberFloat(1, lambda_w / (long double)4, 2);
 	}
 
 	RotGraph[0].z = 0;
 	RotGraph[0].r = 0;
-	RotGraph[29].z = (long double)3 * real(lambda_w);
+	RotGraph[29].z = (long double)3 * lambda_w;
 	RotGraph[29].r = 0;
 
 	//
@@ -138,9 +252,9 @@ int main() {
 	//
 
 
-	std::cout << k1 << "\n";
-	std::cout << k2 << "\n";
-    std::cout << k3 << "\n\n\n\n";
+	//std::cout << z << "\n";
+	//std::cout << k2 << "\n";
+    //std::cout << k3 << "\n\n\n\n";
 
 	//std::cout << k0 << "\n";
 	//std::cout << eta("2", 11) - eta("3", 11) << "\n";
@@ -148,23 +262,23 @@ int main() {
 	std::ofstream out;          // поток для записи
 	out.open("hello.txt");
 
-	std::cout << u(123213, 213123) << "\n";
-	std::cout << u(0, 0) << "\n\n\n\n!!!\n";
+	//std::cout << u(123213, 213123) << "\n";
+	//std::cout << u(0, 0) << "\n\n\n\n!!!\n";
 	std::complex<long double> s = 0;
 	out << "\n\n\n";
-	for (int i = 0; i < 1000; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		out << i << '\n';
 	}
 	out << "real\n";
-	for (int i = 0; i < 1000; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		out << real(u(i, 0.5)) << '\n';
+		//out << real(u(i, 0.5)) << '\n';
 	}
 	out << "imag\n";
-	for (int i = 0; i < 1000; i++)
+	for (int i = 0; i < 10; i++)
 	{
-		out << imag(u(i, 0.5)) << '\n';
+		//out << imag(u(i, 0.5)) << '\n';
 	}
 	
 
